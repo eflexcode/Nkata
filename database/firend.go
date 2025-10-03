455a9fae3107e09e2847ecb0fb7471a7a9b4a330
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2025-10-03 22:26:46 +0100

    working on friends

--- database/firend.go
+++ database/firend.go
@@ -1,14 +1,17 @@
 package database
 
-type Firend struct {
+type Firends struct {
 	ID        int64  `json:"id"`
-	UserID   int64  `json:"user_id"`
+	UserID    int64  `json:"user_id"`
 	FirendID  int64  `json:"friend_id"`
 	CreatedAt string `json:"created_at"`
 }
 
 type FriendRequest struct {
-	ID     int64 `json:"id"`
-	SentBy int64 `json:"sent_by"`
-	SentTo int64 `json:"sent_to"`
+	ID        int64  `json:"id"`
+	SentBy    int64  `json:"sent_by"`
+	SentTo    int64  `json:"sent_to"`
+	Accepted  bool   `json:"accepted"`
+	CreatedAt string `json:"created_at"`
 }
+
