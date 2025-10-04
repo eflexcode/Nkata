851eb167757e7c13cd41cc6b2ea1bfac5575a01e
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2025-10-04 04:30:22 +0100

    friend request db schema

--- database/firend.go
+++ database/firend.go
@@ -1,9 +1,15 @@
 package database
 
-type Firends struct {
+import (
+	"context"
+	"errors"
+)
+
+type Friendship struct {
 	ID        int64  `json:"id"`
 	UserID    int64  `json:"user_id"`
 	FirendID  int64  `json:"friend_id"`
+	Status    string `json:"status"` //blocked, unblocked
 	CreatedAt string `json:"created_at"`
 }
 
@@ -11,7 +17,125 @@ type FriendRequest struct {
 	ID        int64  `json:"id"`
 	SentBy    int64  `json:"sent_by"`
 	SentTo    int64  `json:"sent_to"`
-	Accepted  bool   `json:"accepted"`
+	Status    string `json:"status"` // accepted, pendding, rejected
 	CreatedAt string `json:"created_at"`
 }
 
+func (r *DataRepository) InsertFriendRequest(ctx context.Context, sentTo, sentBy int64) error {
+
+	query := `INSERT INTO friendRequest(sent_by,sent_to,status) VALUES($1,$2,$3)`
+
+	_, err := r.db.ExecContext(ctx, query, sentBy, sentTo, "pending")
+
+	return err
+}
+
+// request i (client) sent out
+func (r *DataRepository) GetFriendRequestSentBy(ctx context.Context, sentBy int64) ([]FriendRequest, error) {
+
+	var request []FriendRequest
+
+	query := `SELECT * FROM friendRequest WHERE sent_by = $1 AND status = $2`
+
+	row, err := r.db.Query(query, sentBy, "pending")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer row.Close()
+
+	for row.Next() {
+
+		item := FriendRequest{}
+
+		err := row.Scan(&item.ID, &item.SentBy, &item.SentTo, &item.Status, &item.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		request = append(request, item)
+
+	}
+
+	return request, nil
+}
+
+// request i (client) was sent
+func (r *DataRepository) GetFriendRequestSentTo(ctx context.Context, sentTo int64) ([]FriendRequest, error) {
+	var request []FriendRequest
+
+	query := `SELECT * FROM friendRequest WHERE sent_to = $1 AND status = $2`
+
+	row, err := r.db.Query(query, sentTo, "pending")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer row.Close()
+
+	for row.Next() {
+
+		item := FriendRequest{}
+
+		err := row.Scan(&item.ID, &item.SentBy, &item.SentTo, &item.Status, &item.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		request = append(request, item)
+
+	}
+
+	return request, nil
+}
+
+func (r *DataRepository) GetFriendRequestById(ctx context.Context, id int64) (*FriendRequest, error) {
+
+	query := `SELECT * FROM friendRequest WHERE ido = $1`
+
+	row, err := r.db.Query(query, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer row.Close()
+
+	row.Next()
+	item := FriendRequest{}
+
+	err = row.Scan(&item.ID, &item.SentBy, &item.SentTo, &item.Status, &item.CreatedAt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &item, nil
+}
+
+func (d *DataRepository) UpdateFriendRequestStatus(ctx context.Context, status string, sentToId,id int64) error {
+
+	if status != "accepted" && status != "rejected" {
+		return errors.New("status can either be accepted or rejected only")
+	}
+
+	f,err := d.GetFriendRequestById(ctx,id)
+
+	if err != nil {
+		return err
+	}
+
+	if f.SentTo != sentToId {
+		return errors.New("unauthorized: sentToId is not same")
+	}
+
+	query := `UPDATE friendRequest SET status = $1 WHERE id = $2`
+
+	_, err = d.db.ExecContext(ctx, query,status,id)
+
+	return err
+}
