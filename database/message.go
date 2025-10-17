cc80427cc415bb8785c9519523cb5c6b24ace9ab
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2025-10-17 23:23:59 +0100

    did insert message

--- database/message.go
+++ database/message.go
@@ -1,6 +1,10 @@
 package database
 
-import "time"
+import (
+	"context"
+	"errors"
+	"time"
+)
 
 type Seen struct {
 	ID         int64     `json:"message_id"`
@@ -20,8 +24,8 @@ const (
 )
 
 type Media struct {
-	MediaUrl  string    `json:"media_url"`
-	MediaType MediaType `json:"media_type"`
+	MediaUrl  string `json:"media_url"`
+	MediaType string `json:"media_type"` // NoMedia,Image,Video,Audio
 }
 
 type MessageType int
@@ -33,13 +37,42 @@ const (
 )
 
 type Message struct {
-	ID           int64       `json:"message_id"`
-	FriendshipID int64       `json:"friendship_id"` //put groupd id here if group
-	SenderID     string      `json:"sender_id"`
-	MessageChat  string      `json:"message_chat"`
-	MessageType  MessageType `json:"message_type"`
-	Media        Media       `json:"media"`
-	Seen         Seen        `json:"seen"`
-	CreatedAt    string      `json:"created_at"`
-	ModifiedAt   string      `json:"modified_at"`
+	ID             int64  `json:"message_id"`
+	FriendshipID   int64  `json:"friendship_id"` //put groupd id here if group
+	SenderUsername string `json:"sender_username"`
+	MessageType    string `json:"message_type"` //MessageChat,MessageRaction,MessageInfo
+	TextContent    string `json:"text_content"`
+	Media          Media  `json:"media"`
+	CreatedAt      string `json:"created_at"`
+	ModifiedAt     string `json:"modified_at"`
+}
+
+func (d *DataRepository) InsertMessage(cxt context.Context, FriendshipID, SenderUsername, MessageType, TextContent string) error {
+
+	query := `INSERT INTO message(friendship_id,sender_username,message_type,text_content,media_url,media_type,modified_at)`
+
+	if MessageType != "MessageChat" && MessageType != "MessageRaction" && MessageType != "MessageInfo" {
+		return errors.New("MessageType is invalide")
+	}
+
+	_, err := d.db.ExecContext(cxt, query, FriendshipID, SenderUsername, MessageType, TextContent,"","NoMedia", time.Now())
+
+	return err
+}
+
+func (d *DataRepository) InsertMessageMedia(cxt context.Context, FriendshipID, SenderUsername, MessageType, TextContent,MediaUrl,MediaType string) error {
+
+	query := `INSERT INTO message(friendship_id,sender_username,message_type,text_content,media_url,media_type,modified_at)`
+
+	if MessageType != "MessageChat" && MessageType != "MessageRaction" && MessageType != "MessageInfo" {
+		return errors.New("MessageType is invalide")
+	}
+
+	if MediaType != "NoMedia" && MediaType != "Image" && MediaType != "Audio" && MediaType != "Video"{
+		return errors.New("MediaType is invalide")
+	}
+
+	_, err := d.db.ExecContext(cxt, query, FriendshipID, SenderUsername, MessageType, TextContent,MediaUrl,MediaType, time.Now())
+
+	return err
 }
