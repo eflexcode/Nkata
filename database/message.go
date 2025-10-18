99b025728c7610dd5a2ae6c468ba0acee82be717
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2025-10-18 03:08:12 +0100

    delete, get and get message by friendship id done

--- database/message.go
+++ database/message.go
@@ -25,7 +25,7 @@ const (
 
 type Media struct {
 	MediaUrl  string `json:"media_url"`
-	MediaType string `json:"media_type"` // NoMedia,Image,Video,Audio
+	MediaType string `json:"media_type"` // NoMedia,Image,Video,Audio,Doc
 }
 
 type MessageType int
@@ -55,12 +55,12 @@ func (d *DataRepository) InsertMessage(cxt context.Context, FriendshipID, Sender
 		return errors.New("MessageType is invalide")
 	}
 
-	_, err := d.db.ExecContext(cxt, query, FriendshipID, SenderUsername, MessageType, TextContent,"","NoMedia", time.Now())
+	_, err := d.db.ExecContext(cxt, query, FriendshipID, SenderUsername, MessageType, TextContent, "", "NoMedia", time.Now())
 
 	return err
 }
 
-func (d *DataRepository) InsertMessageMedia(cxt context.Context, FriendshipID, SenderUsername, MessageType, TextContent,MediaUrl,MediaType string) error {
+func (d *DataRepository) InsertMessageMedia(cxt context.Context, FriendshipID, SenderUsername, MessageType, TextContent, MediaUrl, MediaType string) error {
 
 	query := `INSERT INTO message(friendship_id,sender_username,message_type,text_content,media_url,media_type,modified_at)`
 
@@ -68,11 +68,85 @@ func (d *DataRepository) InsertMessageMedia(cxt context.Context, FriendshipID, S
 		return errors.New("MessageType is invalide")
 	}
 
-	if MediaType != "NoMedia" && MediaType != "Image" && MediaType != "Audio" && MediaType != "Video"{
+	if MediaType != "NoMedia" && MediaType != "Image" && MediaType != "Audio" && MediaType != "Video" && MediaType != "Doc" {
 		return errors.New("MediaType is invalide")
 	}
 
-	_, err := d.db.ExecContext(cxt, query, FriendshipID, SenderUsername, MessageType, TextContent,MediaUrl,MediaType, time.Now())
+	_, err := d.db.ExecContext(cxt, query, FriendshipID, SenderUsername, MessageType, TextContent, MediaUrl, MediaType, time.Now())
 
 	return err
+}
+
+func (d *DataRepository) DeleteMessageById(cxt context.Context, id int) error {
+
+	query := `DELETE FROM message WHERE id = $1`
+	_, err := d.db.ExecContext(cxt, query, id)
+
+	return err
+}
+
+func (d *DataRepository) GetMessageById(cxt context.Context, id int) (*Message, error) {
+
+	query := `SELECT * FROM message WHERE id = $1`
+
+	row, err := d.db.QueryContext(cxt, query, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	row.Next()
+
+	var message Message
+
+	row.Scan(&message.ID, &message.FriendshipID, &message.SenderUsername, &message.TextContent, &message.Media.MediaUrl, &message.Media.MediaType, &message.CreatedAt, &message.ModifiedAt)
+
+	return &message, nil
+}
+
+func (d *DataRepository) GetMessages(cxt context.Context, FriendshipID string, page, limit int) (*PaginatedResponse, error) {
+
+	var totalCount int
+	offset := (page - 1) * limit
+	query := `SELECT * FROM message WHERE friendship_id = $1 LIMIT = $2 OFFSET = $3 ORDER BY created_at DESC`
+	queryCount := `SELECT COUNT(*) FROM message WHERE friendship_id = $1`
+
+	counrRow := d.db.QueryRowContext(cxt, queryCount, FriendshipID)
+
+	err := counrRow.Scan(&totalCount)
+
+	if err != nil {
+		return nil, err
+	}
+
+	row, err := d.db.QueryContext(cxt, query, FriendshipID, limit, offset)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var messages []Message
+
+	for row.Next() {
+
+		var message Message
+
+		row.Scan(&message.ID, &message.FriendshipID, &message.SenderUsername, &message.TextContent, &message.Media.MediaUrl, &message.Media.MediaType, &message.CreatedAt, &message.ModifiedAt)
+		messages = append(messages, message)
+	}
+
+	s := PaginatedResponse{
+		Data:       messages,
+		TotalCount: totalCount,
+		Page:       page,
+		Limit:      limit,
+	}
+	return &s, nil
+
+}
+
+func (d *DataRepository) SearchMessages(cxt context.Context, FriendshipID, search, start_at, end_at string, page, limit int) (*PaginatedResponse, error) {
+
+	
+
 }
